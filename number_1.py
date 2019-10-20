a2d8914c6392298bbf04889fbab9e233949a4ee8
KateYudova <50332825+KateYudova@users.noreply.github.com>
2019-10-21 00:18:40 +0300

    Update number_1.py

--- number_1.py
+++ number_1.py
@@ -7,7 +7,6 @@ import number_0
 import warnings
 warnings.filterwarnings('ignore')
 ls = []
-k = 0
 number_0.srch(os.getcwd(), ls)
 start = time.time()
 if len(ls) == 0:
@@ -16,6 +15,4 @@ if len(ls) == 0:
 print(ls)
 for i in ls:
     number_0.get(i)
-    print(k)
-    k += 1
-print("Finish in %s seconds" % (time.time() - start))
+print("Consistently: Finish in %s seconds" % (time.time() - start))
