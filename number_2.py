0e8f8ff957b68c328aab5a8331ec1572ac54de1b
KateYudova <50332825+KateYudova@users.noreply.github.com>
2019-10-21 00:41:52 +0300

    Create number_2.py

---
+++ number_2.py
@@ -0,0 +1,19 @@
+"""
+threads
+"""
+import os
+import time
+import number_0
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import warnings
+warnings.filterwarnings('ignore')
+ls = []
+number_0.srch(os.getcwd(), ls)
+start = time.time()
+if not len(ls):
+    print ("No files in this directory")
+    exit(0)
+with ThreadPoolExecutor(max_workers=8) as pool:
+    for item in ls:
+        pool.submit(number_0.get, item)
+print("Threads: Finish in %s seconds" % (time.time() - start))
